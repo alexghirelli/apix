ec645551fd0df76d2556d7e895887aaaf93cb8b1
Alex Ghirelli <info@alexghirelli.it>
2022-04-04 02:28:08 +0200

    Added conversion of expiration to epoch

--- server/slack/handler/changeStatus.py
+++ server/slack/handler/changeStatus.py
@@ -3,10 +3,15 @@ import profile
 from urllib import response
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
+from datetime import datetime, timedelta
 from server.slack.model import StatusSchema
 from server.slack.init import client
 
 def changeStatus(status: StatusSchema):
+    expiration = datetime.now() + timedelta( hours = status.status_expiration )
+    
+    status.status_expiration = expiration.timestamp()
+
     new_status = jsonable_encoder(status)
     response = client.users_profile_set(profile = new_status)
 
--- server/slack/model.py
+++ server/slack/model.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel
 class StatusSchema(BaseModel):
     status_text: str
     status_emoji: str
-    status_expiration: str
+    status_expiration: int
